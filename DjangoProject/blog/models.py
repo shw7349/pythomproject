c1805b8b4282f77b82c02ee790d43b2518e64ff8
HyunWoo <shw7349@naver.com>
2023-01-31 23:31:35 +0900

    day1

--- DjangoProject/blog/models.py
+++ DjangoProject/blog/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
-# Create your models here.
 class Post(models.Model):
-    # 50자 까지 가능하다
     title = models.CharField(max_length=50)
     content = models.TextField()
-    
-    # 시간을 저장하는기능
-    created_at = models.DateTimeField()
-    # author 
+
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # author : 추후 작성예정
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
